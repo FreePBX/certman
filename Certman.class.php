ce43dc115cbde56a4ed789f956ffc2e2a77486b7
Andrew Nagy <andrew.nagy@the159.com>
2014-12-03 14:26:57 -0500

    FREEI-253

--- Certman.class.php
+++ Certman.class.php
@@ -1,11 +1,8 @@
 <?php
 // vim: set ai ts=4 sw=4 ft=php:
-/**
- * This is the Certificate Manager Object.
- *
- * License for all code of this FreePBX module can be found in the license file inside the module directory
- * Copyright 2006-2014 Schmooze Com Inc.
- */
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2014 Schmooze Com Inc.
+//
 
 class Certman implements BMO {
 	/* Asterisk Defaults */
--- functions.inc.php
+++ functions.inc.php
@@ -1,2 +1,5 @@
 <?php
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2014 Schmooze Com Inc.
+//
 include_once(dirname(__FILE__) . '/functions.inc/hook_core.php');
