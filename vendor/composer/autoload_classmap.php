d89b86060ea30c115070b69c2f935110684df15c
Andrew Nagy <andrew.nagy@the159.com>
2018-05-01 15:05:30 -0400

    [Module Tag script: certman 13.0.37.6]

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -4,6 +4,7 @@
 
 $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
+
 return array(
     'Composer\\CaBundle\\CaBundle' => $vendorDir . '/composer/ca-bundle/src/CaBundle.php',
 );
