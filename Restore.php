f882a3eb0ba15e13b4a825905731cd3a5e711c38
James Finstrom <jfinstrom@sangoma.com>
2018-07-14 18:08:01 +0000

    Restore cleanup and testing

--- Restore.php
+++ Restore.php
@@ -4,6 +4,7 @@ use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
     $configs = $this->getConfigs();
+    $configs = reset($configs);
     $files = $this->getFiles();
     $dirs = $this->getDirs();
     $this->certman = $this->FreePBX->Certman;
@@ -17,12 +18,16 @@ class Restore Extends Base\RestoreBase{
       $backupFilename = $file['pathto'] . '/' . $file['filename'];
       $dir = str_Replace($originalKeyDir, $keyDir, $file['pathto']);
       $newFilename = $dir . '/' . $file['filename'];
-        if (file_exists($backupFilename)) {
-          copy($this->tmpdir . '/files/' . $file['pathto'] . '/' . $file['filename'], $newFilename);
+        if (file_exists($this->tmpdir.'/files/'.$backupFilename)) {
+         copy($this->tmpdir . '/files/' . $file['pathto'] . '/' . $file['filename'], $newFilename);
         }
     }
     foreach($configs['managedCerts'] as $cert){
-      $this->certman->saveCertificate($cert['caid'], $cert['basename'], $cert['description'], $cert['type'], $cert['additional']);
+      try{
+	      $this->certman->saveCertificate($cert['caid'], $cert['basename'], $cert['description'], $cert['type'], $cert['additional']);
+      }catch(\Exception $e){
+ 	echo $e->getMessage().PHP_EOL;
+      }
     }
 
     foreach($configs['managedCSRs'] as $csr){
