bcfcbd0e47f32318edb500bdcdeba3147b696ec9
Vivek Mishra <vmishra@sangoma.com>
2020-06-22 03:49:09 +0000

    FREEPBX-21600 fixing -SRTP selection was Enabling DTLS

--- assets/js/views/extensionsHook.js
+++ assets/js/views/extensionsHook.js
@@ -7,11 +7,13 @@ document.addEventListener('DOMContentLoaded', function(ev) {
 				$("input[name='dtls_enable']:checked").val('no');
 				return warnInvalid("#devinfo_media_encryption",_("Please enable DTLS for Media Encryption field first before enabling DTLS parameters"));
 			}
-	   	}
+	   	}else{
+			$( "#devinfo_media_encryption option:selected" ).val('no');
+		}
 	});
 
 	$("#devinfo_media_encryption").change(function() {
-		if($(this).val() != 'dtls' && $("input[name='dtls_enable']:checked").val() == 'yes'){
+		if($(this).val() != 'dtls'){
 			$("input[name='dtls_enable']:checked").val('no');
 		}else{
 			$("input[name='dtls_enable']:checked").val('yes');
