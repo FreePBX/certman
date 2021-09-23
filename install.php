995a64cf6b8b11afe768829956005738990d6e92
Matt Brooks <mbrooks@sangoma.com>
2021-09-23 20:55:19 +0000

    FREEPBX-22849 Fix for Let's encrypt root CA
    On Oct 1st, one of Let's encrypt CA root certs is expiring. This PR
    provides a fix to remove the older CA cert so that we don't have any
    certificate issues when connecting to servers signed by Let's encrypt.

--- install.php
+++ install.php
@@ -30,3 +30,14 @@ $set['name'] = 'Validity period of the certificate (in days)';
 $set['description'] = 'You can change the number of days the certificate is valid.';
 $set['type'] = CONF_TYPE_TEXT;
 $freepbx_conf->define_conf_setting('CERT_DAYS_VAL',$set,true);
+
+// Fix Let's Encrypt DST-Root-CA-X3 issue
+$m = \module_functions::create();
+$distro = $m->_distro_id();
+
+// Only run this on SNG7
+if ($distro['pbx_type'] === "freepbxdistro" && FreePBX::Modules()->checkStatus("sysadmin")) {
+	if (!file_exists('/etc/pki/ca-trust/source/blacklist/DST-Root-CA-X3.pem')) {
+		FreePBX::Certman()->runHook("fix-le-root-ca");
+	}
+}
