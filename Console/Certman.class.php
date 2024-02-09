e458330ec834e2ca6c02a69ad90e44d15a537a22
Michael Newton <miken32@gmail.com>
2024-02-09 13:12:55 -0700

    fix undefined variable notice and PHP 8 warning

--- Console/Certman.class.php
+++ Console/Certman.class.php
@@ -59,7 +59,7 @@ class Certman extends Command {
 
 				case 'letsencrypt';
 				case 'le':
-					$hostname = strtolower($input->getOption('hostname'));
+					$hostname = strtolower((string)$input->getOption('hostname'));
 					$country_code = $input->getOption('country-code');
 					$state = $input->getOption('state');
 					$email = $input->getOption('email');
@@ -237,6 +237,7 @@ class Certman extends Command {
 			}
 			$messages = $certman->checkUpdateCertificates($force);
 			$hints = array();
+			$danger = false;
 			foreach($messages as $message) {
 				if (!empty($message['hints'])) {
 					$hints = array_merge($hints, $message['hints']);
